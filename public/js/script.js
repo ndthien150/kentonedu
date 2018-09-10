9d4f58a47008df2b2acade15d441b07baa0a7d7b
Thien <ndthien150@gmail.com>
2018-09-11 01:46:50 +0700

    cap nhat home page

--- public/js/script.js
+++ public/js/script.js
@@ -12,16 +12,16 @@
             }
         });
 
-        // $('#myCarousel').swipe({
+        $('#myCarousel').swipe({
 
-        //     swipe: function (event, direction, distance, duration, fingerCount, fingerData) {
+            swipe: function (event, direction, distance, duration, fingerCount, fingerData) {
 
-        //         if (direction == 'left') $(this).carousel('next');
-        //         if (direction == 'right') $(this).carousel('prev');
-        //     },
-        //     allowPageScroll: "vertical"
+                if (direction == 'left') $(this).carousel('next');
+                if (direction == 'right') $(this).carousel('prev');
+            },
+            allowPageScroll: "vertical"
 
-        // });
+        });
 
 
         /* Menu mobile */
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -5,5 +5,35 @@
 @endsection
 
 @section('content')
-    <h1>This is home page</h1>
+    <section class="container-fuild">
+        <div class="container-fuild">
+            <div id="myCarousel" class="carousel slide" data-ride="carousel" data-interval="3000">
+            <!-- Indicators -->
+            <ol class="carousel-indicators">
+                <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                <li data-target="#myCarousel" data-slide-to="1"></li>
+            </ol>
+
+            <!-- Wrapper for slides -->
+            <div class="carousel-inner">
+                <div class="item active">
+                    <img src="{{ asset('images/duhocKenton-slide1.jpg')}}" alt="Los Angeles">
+                </div>
+
+                <div class="item">
+                    <img src="{{ asset('images/duhocKenton-slider2.jpg')}}" alt="Chicago">
+                </div>
+            </div>
+
+            <!-- Left and right controls -->
+            <!-- <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+                <span class="glyphicon glyphicon-chevron-left"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="right carousel-control" href="#myCarousel" data-slide="next">
+                <span class="glyphicon glyphicon-chevron-right"></span>
+                <span class="sr-only">Next</span>
+            </a> -->
+        </div>
+    </div>
 @endsection
